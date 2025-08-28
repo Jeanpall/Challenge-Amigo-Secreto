ed29250227a82f13da79e72f6ae75ae6c9901c9c
Jeanpall Ortega Izquierdo <auxiliarit@EDEXA.COM.CO>
2025-08-28 16:59:47 -0500

    Definicion de funcionalidad Inicial

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Implementar la Funcion para agregar
+let nombreAmigo = '';
+let listaAmigos = [];
+
+
+function agregarAmigo() {
+    console.log(document.getElementById('amigo').value);
+}
+
+function sortearAmigo() {
+
+}
